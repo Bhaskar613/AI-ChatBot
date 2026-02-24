8e6d3722bdd4fa5121f6bcdb549823ca9619ffb6
Bhaskar613 <tonangibhaskararao@gmail.com>
2026-02-24 22:23:46 +0530

    api file update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API = "https://ai-chatbot-backend-pss2.onrender.com";
+const BASE_URL = "https://ai-chatbot-backend-pss2.onrender.com/api";
 
 export const sendMessageAPI = (sessionId, message) => {
-  return axios.post(`${API}/chat`, { sessionId, message });
+  return axios.post(`${BASE_URL}/chat`, { sessionId, message });
 };
 
 export const fetchConversationAPI = (sessionId) => {
-  return axios.get(`${API}/conversations/${sessionId}`);
+  return axios.get(`${BASE_URL}/conversations/${sessionId}`);
 };
